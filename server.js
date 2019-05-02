10297edd0515fd2b82eef3808f89561db14d69bb
matthew-trump <matthew.trump@gmail.com>
2019-05-02 10:48:23 -0700

    executable file

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+//const ping = require('./ping');
+//const { jwtAuthorization, jwtLogin, jwtUnauthorizedError } = require('./jwt-auth');
+//const api = require("./api");
+
+const flightTracker = require('./flight-tracker');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+/**
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/ping", ping);
+
+app.use("/login", jwtLogin);
+
+app.use("/api",
+    jwtAuthorization,
+    jwtUnauthorizedError,
+    api);
+ */
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log("");
+    console.log("FLIGHT TRACKER SERVER APPLICATION");
+    console.log(`listening on port ${PORT}`);
+    console.log("ENVIRONMENT", process.env.ENVIRONMENT);
+
+});
+;
