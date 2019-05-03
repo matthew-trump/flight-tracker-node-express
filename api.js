f37d6720f721dc2e0703ceb925f3af017b502c32
matthew-trump <matthew.trump@gmail.com>
2019-05-03 09:39:36 -0700

    move logic to flight tracker object

--- api.js
+++ api.js
@@ -1,34 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-var toobusy = require('node-toobusy');
-
-
-
-const CONFIG_FILE = process.env.CONFIG_FILE;
-
-
-
-
-const DATABASE_URL = process.env.DATABASE_URL;
-
-
-
-const originalConfig = JSON.parse(JSON.stringify(events));
-
-
-
-const config = require(CONFIG_FILE);
-const thresholdNormal = 60000; //one minute
-const thresholdEnroute = 10000; //ten seconds
-const preBuffer = 3600000 * 48;
-const postBuffer = 3600000 * 2;
-
-const databaseURL = DATABASE_URL;
-
-
-
-
+const toobusy = require('node-toobusy');
+const FlightTracker = require('./flight-tracker');
 
 
 router.use(function (req, res, next) {
@@ -44,19 +17,17 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/config/events', function (req, res, next) {
-    return res.json(originalConfig);
+    return res.json(FlightTracker.originalConfig);
 });
 
 router.get('/config/event/:eventId', function (req, res, next) {
-    var original = originalConfig[req.params.eventId];
+    var original = FlightTracker.originalConfig[req.params.eventId];
     if (original) {
         return res.json(original);
     } else {
         return res.json({ message: "INVALID EVENT ID" });
     }
-
 });
-
 
 router.get('/tracker/:eventId/next', function (req, res, next) {
     var eventId = req.params.eventId;
@@ -77,38 +48,14 @@ router.get('/tracker/:eventId/next', function (req, res, next) {
         time = now;
     }
 
-    if (eventId && (typeof events[eventId] !== 'undefined')) {
-        var event = events[eventId];
-        if (event.active && event.currentFlightIndex >= 0) {
-
-            if (!jsonData[eventId][event.currentFlightIndex]) {
-                updateFromFlightAware(event, time, adjust);
-                res.send({ message: "WAITING FOR UPDATE. PLEASE RELOAD.", time: time, eventId: eventId });
-            } else {
-
-
-                var json = jsonData[eventId][event.currentFlightIndex];
-                var isEnroute = json.departureTime < time && time < json.arrivalTime;
-                if (!isEnroute) {
-                    delete json.enroute;
-                    //delete currentJsonData[eventId][event.currentFlightIndex];
-                } else if (currentJsonData[eventId][event.currentFlightIndex]) {
-                    json.enroute = currentJsonData[eventId][event.currentFlightIndex].enroute;
-
-                    if (!json.enroute) {
-                        json.enroute = {};
-                    }
-                }
-
-                let threshold = isEnroute ? thresholdEnroute : thresholdNormal;
-                if (Date.now() - json.recorded > threshold) {
-                    updateFromFlightAware(event, time, adjust)
-                }
-
-                res.json(json);
-            }
-        } else {
+    if (eventId && (typeof FlightTracker.events[eventId] !== 'undefined')) {
+        const json = FlightTracker.getEnrouteJson(eventId);
+        if (!json) {
+            res.send({ message: "WAITING FOR UPDATE. PLEASE RELOAD.", time: time, eventId: eventId });
+        } else if (json === -1) {
             res.json({ message: "OUTSIDE FLIGHT REPORTING WINDOW FOR EVENT", time: time });
+        } else {
+            res.json(json);
         }
     } else {
         res.status(400).json({ message: "INVALID EVENT ID" });
