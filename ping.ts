e2d3c5a020a44c651686812531b72b8457d569cf
matthew-trump <matthew.trump@gmail.com>
2019-05-03 16:04:19 -0700

    adding package modules for jwt

--- ping.ts
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (_, res) => {
-    res.status(200).json({ message: "OK", value: new Date() });
-});
-
-module.exports = router;
