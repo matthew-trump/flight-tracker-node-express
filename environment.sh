d517cf99cdde95be27e6042c56b7107496aa2a99
matthew-trump <matthew.trump@gmail.com>
2019-05-03 13:29:48 -0700

    template for environment file

--- environment.sh
+++ environment.sh
@@ -5,4 +5,8 @@ export FLIGHT_AWARE_API_KEY="---your-flight-aware-apiKey--"
 export FIREBASE_DATABASE_URL="https://flight-tracker-54485.firebaseio.com"
 export FIREBASE_SERVICE_ACCOUNT="./firebase-service-account.json"
 
+export ADMIN_USERNAME="admin"
+export ADMIN_PASSWORD="---admin-123---"
+export ADMIN_SESSION_EXPIRY_IN_SECONDS=2592000
+
 export EVENTS_FILE="./events.json"
